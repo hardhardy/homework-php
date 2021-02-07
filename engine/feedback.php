70d736e90eb2a606ee19bbca40a5af50c3a14657
Veniamin <ma3ujia@list.ru>
2021-02-07 14:52:14 +0500

    Feedback abrakadabra

--- engine/feedback.php
+++ engine/feedback.php
@@ -1,33 +1,37 @@
 <?php
 
-function getAllFeedback() {
-    $sql = "SELECT * FROM feedback ORDER BY id DESC";
-    return getAssocResult($sql);
+function getAllFeedback()
+{
+  return getAssocResult("SELECT * FROM feedback ORDER BY id DESC");
 }
 
-function addFeedBack() {
-    var_dump($_POST);
-    die();
+function addFeedBack()
+{
+  if ($_POST) {
+    $name = strip_tags(htmlspecialchars(mysqli_real_escape_string($db, $_POST['name'])));
+    $text = strip_tags(htmlspecialchars(mysqli_real_escape_string($db, $_POST['text'])));
+  }
 }
 
-function deleteFeedBack() {
-    var_dump($_POST);
-    die();
+function deleteFeedBack()
+{
+  var_dump($_POST);
+  die();
 }
 
-function doFeedbackAction($action) {
-    if ($action = "add") {
-        addFeedBack();
-    }
-
-    if ($action = "edit") {
-        addFeedBack();
-    }
-    if ($action = "delete") {
-        deleteFeedBack();
-    }
-    if ($action = "save") {
-        addFeedBack();
-    }
+function doFeedbackAction($action)
+{
+  if ($action = "add") {
+    addFeedBack();
+  }
+  if ($action = "edit") {
+    addFeedBack();
+  }
+  if ($action = "delete") {
+    deleteFeedBack();
+  }
+  if ($action = "save") {
+    addFeedBack();
+  }
 }
 
--- templates/feedback.php
+++ templates/feedback.php
@@ -3,11 +3,11 @@
 <form action="/?action=<?=$action?>" method="post">
   <input hidden type="text" name="id" value="<?=$row['id']?>"><br>
   <input type="text" name="name" value="<?=$row['name']?>"><br>
-  <input type="text" name="feedback" value="<?=$row['feedback']?>"><br>
-  <input type="submit" value="<?=$buttonText?>">
+  <input type="text" name="text" value="<?=$row['feedback']?>"><br>
+  <input type="submit" value="Отправить<?=$buttonText?>">
 </form>
 <?php foreach ($result as $feedback):?>
   <p>
-    <b><?=$feedback['name']?>:</b><?=$feedback['feedback']?> <a href="/?action=edit&id=<?=$feedback['id']?>">[edit]</a>  [x]
+    <b><?=$feedback['name']?>:</b><?=$feedback['text']?> <a href="/?action=edit&id=<?=$feedback['id']?>">[edit]</a>  [x]
   </p>
 <?endforeach;?>
